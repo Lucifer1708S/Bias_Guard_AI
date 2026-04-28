c12712851f4a67bc5bf50d9442948004423b7f97
Lucifer1708S <Sathyaperumal1708@gmail.com>
2026-04-29 00:33:06 +0530

    Saved

--- main.py
+++ main.py
@@ -178,8 +178,7 @@ app = FastAPI(title="AI Bias Monitoring System", version="1.0.0")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://bias-guard-ui.onrender.com","*"],],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://bias-guard-ui.onrender.com","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
